fc0ad719be280b34c0b15e565b8728cf92cc300a
Noah Olsen <noah.olsen@slalom.com>
2025-04-28 15:54:33 -0600

    feat: adds autoscaling 3

--- main.tf
+++ main.tf
@@ -43,9 +43,15 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arn    = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
+  traffic_source_attachments = {
+    blog_alb = {
+      traffic_source_identifier = module.blog_alb.target_groups["blog_asg"].arn
+      traffic_source_type = "elbv2"
+    }
+  }
+  
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
